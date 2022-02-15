5c5fb0c3616b456c4af4807cc91cf1b81c90aa29
Felipe Alberto <fealberto@gmail.com>
2022-02-14 19:22:27 -0500

    Added infrastructure + database

---
+++ src/Pokedex.Infrastructure/Persistence/Context/PokedexDbContext.cs
@@ -0,0 +1,20 @@
+﻿global using Microsoft.EntityFrameworkCore;
+using Pokedex.Domain.Pokemon;
+
+namespace Pokedex.Infrastructure.Persistence.Context;
+
+public sealed class PokedexDbContext : DbContext
+{
+    public DbSet<Pokemon> Pokemon { get; set; }
+
+    public PokedexDbContext(DbContextOptions<PokedexDbContext> options) 
+        : base(options)
+    {
+        Database.EnsureCreated();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(PokedexDbContext).Assembly);
+    }
+}
